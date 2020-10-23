5b8b20b9c32a60f650fb0a782642e611fc200c83
git <git@github.com>
2020-10-23 15:56:47 -0300

    cidades e foranias add

--- src/componentes/Dados/dados.js
+++ src/componentes/Dados/dados.js
@@ -7,8 +7,130 @@ module.exports.crato = [
                 funcao:'Presidente Diocesana'
             },
             {
+                nome:'Cícero Antônio',
+                funcao:'Secretário Geral'
+            },
+            {
+                nome:'Vagner Almeida',
+                funcao:'1º Secretário'
+            },
+            {
+                nome:'Acácio Lima',
+                funcao:'2º Secretário'
+            },
+            {
                 nome:'Larissa Parente',
-                funcao:'2ª Tesoureira'
+                funcao:'1ª Tesoureira'
+            },
+            {
+                nome:'Poliana Vieira',
+                funcao:'2ª Tesoureira   '
+            },
+        ]
+    },
+    {
+        titulo:'Coordenadores de Forania',
+        content:[
+            {
+                nome:'Edson Douglas',
+                funcao:'Forania I'
+            },
+            {
+                nome:'Paulo Vieira',
+                funcao:'Forania II'
+            },
+            {
+                nome:'Railson Dias',
+                funcao:'Forania III'
+            },
+            {
+                nome:'Klésia Sá',
+                funcao:'Forania IV'
+            },
+            {
+                nome:'A discernir',
+                funcao:'Forania V'
+            }
+        ]
+    },
+    {
+        titulo:'Coordenadores de Cidade',
+        content:[
+            {
+                nome:'Edson Fernandes',
+                funcao:'Abaiara'
+            },
+            {
+                nome:'Valéria Macêdo',
+                funcao:'Araripe'
+            },
+            {
+                nome:'Áurio Gomes',
+                funcao:'Assaré'
+            },
+            {
+                nome:'Danúbio Lopes',
+                funcao:'Aurora'
+            },
+            {
+                nome:'Rosimyre Santos',
+                funcao:'Baixio'
+            },
+            {
+                nome:'A discernir',
+                funcao:'Barbalha'
+            },
+            {
+                nome:'Fátima Martins',
+                funcao:'Barro'
+            },
+            {
+                nome:'Cícero José',
+                funcao:'Brejo Santo'
+            },
+            {
+                nome:'Gilde Veloso',
+                funcao:'Campos Sales'
+            },
+            {
+                nome:'Edson Douglas',
+                funcao:'Crato'
+            },
+            {
+                nome:'Aparecida Pinheiro',
+                funcao:'Farias Brito'
+            },
+            {
+                nome:'Adeliane Bringel',
+                funcao:'Jati'
+            },
+            {
+                nome:'Suzana Aquino',
+                funcao:'Juazeiro do Norte'
+            },
+            {
+                nome:'Fábio Saraiva',
+                funcao:'Mauriti'
+            },
+            {
+                nome:'Marilene Patrício',
+                funcao:'Missão Velha'
+            },
+            {
+                nome:'Adriana Alves',
+                funcao:'Penaforte'
+            },
+            {
+                nome:'Zenaide da Silva',
+                funcao:'Porteiras'
+            },
+            {
+                nome:'Jonas Pedro',
+                funcao:'Salitre'
+            },
+            {
+                nome:'Joéliton Araújo',
+                funcao:'Várzea Alegre'
             }
         ]
     },
