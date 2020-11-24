d28f5af98a022a77f0cb9a0fe812290cab6720dc
and.moreira5 <and.moreira5@gmail.com>
2020-11-24 11:56:09 -0300

    mapa desenhado

--- src/componentes/Home/Home.js
+++ src/componentes/Home/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './estilo.css';
 const dados = require('../Dados/Mapa');
+
+
 export default function Home(){
     
-
     function lerDados(dado){
         return <Link class="link" to={dado.endereco}>
              <path d={dado.path} />
@@ -14,7 +15,7 @@ export default function Home(){
     return(
         <div id="home" class="center">
                 <h3>Bem vindo(a)!</h3>
-                <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 3000 3000">
+                <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 2000 3000">
                     {dados.map(a => lerDados(a))}                    
                 </svg>
         </div>
